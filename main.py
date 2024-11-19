a9ea53fdb341ca7dac1231f38be4fa48b96853be
tmazko <tmazko@kse.org.ua>
2024-11-19 16:20:05 +0200

    add arguments_validation and print_medalists functions

--- main.py
+++ main.py
@@ -2,15 +2,42 @@ import os
 import argparse
 import sys
 
+LIST_NUM = 10
+
 def get_data_from_file(name):
     data = []
     with open(os.getcwd() + fr"\{name}", "r") as file:
         next(file)
         for row in file:
-            a = [int(i) if i.isdigit() else i for i in row[:-1].split(";")]
+            a = [int(i) if i.isdigit() else i.replace('"','') for i in row[:-1].split(",")]
             data.append(a)
     return data
 
+def arguments_validation(input_file, medalist, country, year, output_file):
+    if not os.path.exists(input_file):
+        print("There is no such input file")
+        return False
+
+    year_flag = country_flag= False
+    for i in range(len(data)):
+        if country == data[i][6] or country == data[i][7]:
+            country_flag=True
+        if year == data[i][9]:
+            year_flag = True
+        if year_flag and country_flag:
+            return True
+    return False
+
+
+def print_medalists(country, year):
+    counter = 0
+    for i in range(len(data)):
+        if counter < 10:
+            if (data[i][6] == country or data[i][7]==country)and year==data[i][9]:
+                print(*data[i])
+                counter+=1
+        else:
+            break
 
 parser=argparse.ArgumentParser()
 parser.add_argument('input', help="Filepath for an input file")
@@ -21,16 +48,11 @@ parser.add_argument('-output',type=str, help="Filepath for an output file")
 
 args=parser.parse_args()
 
+data = get_data_from_file("athlete_events.csv")
 
-"""
-print ("The path is ", args.input)
-print ("Flag value is ", args.medals)
-print("Country: ", args.country)
-print("Year", args.year)
-print("output file", args.output)
 
-data=get_data_from_file(args.input)
+if arguments_validation(args.input, args.medals, args.country,args.year,args.output):
+    print_medalists(args.country, args.year)
+else:
+    print("Try again")
 
-for i in range(10):
-    print(*data[i])
-"""
