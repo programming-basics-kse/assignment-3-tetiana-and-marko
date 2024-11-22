74a9d75a248016cea3d3b15d20973780be43d6d1
mckaradachi <markokapranov11@gmail.com>
2024-11-22 22:10:47 +0200

    added overall

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 LIST_NUM = 10
 
+
 def get_data_from_file(name):
     data = []
     with open(os.getcwd() + fr"\{name}", "r") as file:
@@ -13,16 +14,28 @@ def get_data_from_file(name):
             data.append(a)
     return data
 
+
 def arguments_validation(input_file, country, year, output_file):
+    year_flag = country_flag = False
     if not os.path.exists(input_file):
         print("There is no such input file")
         return False
-    year_flag = country_flag = False
+
+    if args.overall:
+        for i in country:
+            for j in range(len(data)):
+                if i == data[j][6] or i == data[j][7] or args.total:
+                    country_flag = True
+        year_flag = True
+
     for i in range(len(data)):
-        if country == data[i][6] or country==data[i][7] or args.total:
+        if country == data[i][6] or country == data[i][7] or args.total:
             country_flag = True
         if year == data[i][9]:
             year_flag = True
+
+
+
     if year_flag and country_flag:
         return True
     if not country_flag:
@@ -30,6 +43,7 @@ def arguments_validation(input_file, country, year, output_file):
     if not year_flag:
         print(f"{year} is not in dataset!(Year invalid)")
     return False
+
 
 def total_dictionary(year):
     totals = {}
@@ -42,6 +56,7 @@ def total_dictionary(year):
                 totals[i[6]].append(i[-1])
     return totals
 
+
 def print_total(year):
     totals = total_dictionary(year)
     for j in totals:
@@ -49,12 +64,48 @@ def print_total(year):
             print(
                 f"{j}  Bronze:{totals[j].count('Bronze')} Silver:{totals[j].count('Silver')} Gold:{totals[j].count('Gold')}")
 
+
+def overall_sorter(countries):
+    overall = {}
+    sorted_result = {}
+
+    for c in countries:
+        overall[c] = {}
+        for b in data:
+            if b[6] == c or b[7] == c:
+                year = b[9]
+                medals_count = b.count('Bronze') + b.count('Silver') + b.count('Gold')
+                if year in overall[c]:
+                    overall[c][year] += medals_count
+                else:
+                    overall[c][year] = medals_count
+
+    for c in countries:
+        if overall[c]:
+            year, maxi = max(overall[c].items(), key=lambda x: x[1])
+            sorted_result[c] = {year: maxi}
+
+    return sorted_result
+
+
+
+
+
+def print_overall(countries):
+    overall = overall_sorter(countries)
+    for i in overall:
+        print(f"{i} {list(overall[i].keys())[0]} {list(overall[i].values())[0]}")
+
+def write_overall():
+    pass
+
+
 def print_medalists(country, year):
     counter = 0
     for i in range(len(data)):
         if counter < 10:
             if (data[i][6] == country or data[i][7] == country) and year == data[i][9]:
-                print(f"{data[i][1]}, {data[i][12]}, {data[i][14]}")
+                print(f"{data[i][1]}, {data[i][12]}, {data[i][-1]}")
                 counter += 1
         else:
             break
@@ -80,28 +131,43 @@ def write_output(country, year, type):
                     file.write(
                         f"{j}  Bronze:{totals[j].count('Bronze')} Silver:{totals[j].count('Silver')} Gold:{totals[j].count('Gold')}")
                     file.write("\n")
+        if type == args.overall:
+            overall = overall_sorter(country)
+            for i in overall:
+                file.write(f"{i} {list(overall[i].keys())[0]} {list(overall[i].values())[0]}")
+                file.write("\n")
+
     file.close()
 
-#PARSER
+
+# PARSER
 parser = argparse.ArgumentParser()
 parser.add_argument('input', help="Filepath for an input file")
-parser.add_argument('-medals',nargs=2, help="Argument to get medalists by country and year")
+parser.add_argument('-medals', nargs=2, help="Argument to get medalists by country and year")
 parser.add_argument('-total', help="Argument to get medalists by year")
+parser.add_argument('-overall', nargs='+', help="Argument to get the year with the most medals")
 parser.add_argument('-output', type=str, help="Filepath for an output file")
 
 args = parser.parse_args()
 
-#LOAD DATA
+# LOAD DATA
 data = get_data_from_file("athlete_events.csv")
 
-#MAIN
+# MAIN
 if args.medals:
-    country,year=args.medals
-else:
-    year=args.total
-    country=None
-if year.isdigit():
-    year = int(year)
+    country, year = args.medals
+    if year.isdigit():
+      year = int(year)
+elif args.total:
+    year = args.total
+    country = None
+    if year.isdigit():
+      year = int(year)
+
+elif args.overall:
+    country = args.overall
+    year = None
+
 
 if arguments_validation(args.input, country, year, args.output):
     if args.medals:
@@ -112,5 +178,9 @@ if arguments_validation(args.input, country, year, args.output):
         print_total(year)
         if args.output:
             write_output(country, year, args.total)
+    elif args.overall:
+        print_overall(country)
+        if args.output:
+            write_output(country, year, args.overall)
 else:
     print("Try again")
