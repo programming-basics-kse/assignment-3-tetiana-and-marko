7504b7930410ef2aae4736bfd1d8acd33edc054e
tmazko <tmazko@kse.org.ua>
2024-11-23 12:25:34 +0200

    created interactive mode

--- interactive.py
+++ interactive.py
@@ -13,7 +13,7 @@ def interactive_mode():
         print("You have 4 options: ")
         print("1. -medals (get medalists by country and year)")
         print("2. -total (get medalists by year)")
-        print("3. -")
+        print("3. -overall")
 
 
         if not is_continue():
